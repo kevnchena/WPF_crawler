4e29186ce3e6e2d4670b940a3a3d5e891d25b98d
KevinChena <z6256875@gmail.com>
2023-12-11 10:54:31 +0800

    add class

--- WPF_crawler/MainWindow.xaml.cs
+++ WPF_crawler/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,36 @@ namespace WPF_crawler
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
+        
+        AQIdata aqidata = new AQIdata();
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+        }
+
+        private async void fetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string url = UrlTextBox.Text;
+            ContentTextBox.Text = "正在抓取網路資料...";
+
+            string data = await GetWebContent(url);
+        }
+
+        private async Task<string> GetWebContent(string url)
+        {
+            //try
+            //{
+                using (HttpClient client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            //}
+            //catch(Exception ex)
+            //{
+            //    ContentTextBox.Text = ex.Message;
+            //}
         }
     }
 }
